953da313f643e24daaba764c6d857bdb1b7efeb7
lxr <lxr18655043150@163.com>
2024-11-01 15:20:09 +0800

    auto init V1 done

---
+++ auto.go
@@ -0,0 +1,26 @@
+package auto
+
+import (
+	"log"
+	"os"
+
+	"github.com/LoveCatdd/util/pkg/lib/core/config"
+	"github.com/LoveCatdd/webctx/pkg/lib/core/web/auth"
+	"github.com/joho/godotenv"
+)
+
+// 用于初始化文件配置
+func init() {
+
+	// 相对工作文件路径
+	godotenv.Load("../.env")
+
+	environ := os.Getenv("environment")
+
+	config.SetEnviro(environ)
+
+	config.Yaml(auth.JwtConfig)
+
+	log.Println(auth.JwtConfig.Jwt.Secret)
+
+}
